2f5e1df1f2b7dfb1985385051b33a10bf2a5ba1d
daryal89 <truth_aryal@yahoo.com>
2021-05-10 13:21:19 -0400

    example classes added

---
+++ src/test/java/demo/Example2.java
@@ -0,0 +1,8 @@
+package demo;
+
+public class Example2 {
+	public static void main(String[] args) {
+		System.out.println("Example2 Class");
+	}
+
+}
---
+++ src/test/java/demo/Example3.java
@@ -0,0 +1,8 @@
+package demo;
+
+public class Example3 {
+	public static void main(String[] args) {
+		System.out.println("Example3 Class");
+	}
+
+}
