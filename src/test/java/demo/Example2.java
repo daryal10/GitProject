f3576f5dea112906afdfb2c083e3de37f0665854
daryal89 <truth_aryal@yahoo.com>
2021-05-10 13:41:58 -0400

    change made

--- src/test/java/demo/Example2.java
+++ src/test/java/demo/Example2.java
@@ -1,15 +1,7 @@
 package demo;
 
 public class Example2 {
-<<<<<<< HEAD
-	
 	public static void main(String[] args) {
 		System.out.println("Example2 class Added");
-
-=======
-
-	public static void main(String[] args) {
-		System.out.println("Example2 class Added");
->>>>>>> bb29e976cc8bbb32992b66e93b0af91370638966
 	}
 }
