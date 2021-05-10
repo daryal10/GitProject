273a2d0490b91f8ec62f09a63e75842e4845cc6c
daryal89 <truth_aryal@yahoo.com>
2021-05-10 12:36:24 -0400

    First Commit

---
+++ src/main/java/GitExample/GitExample/App.java
@@ -0,0 +1,13 @@
+package GitExample.GitExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/demo/ClassA.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class ClassA {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Git Concept");
+		
+	}
+
+}
