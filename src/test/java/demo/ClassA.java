582105fa0b1af64dc858187761d99f0f04caa029
daryal89 <truth_aryal@yahoo.com>
2021-05-10 12:49:54 -0400

    Code committed

--- src/test/java/demo/ClassA.java
+++ src/test/java/demo/ClassA.java
@@ -6,6 +6,8 @@ public class ClassA {
 		
 		System.out.println("Git Concept");
 		
+		System.out.println("Git Concept -- GitExample");
+		
 	}
 
 }
---
+++ src/test/java/demo/Example1.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		System.out.println("Example1");
+
+	}
+
+}
